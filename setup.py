294bb6130b999369904e13309e6ef6ea5c5c7e60
aliyome <aliyome@gmail.com>
2020-02-27 23:54:32 +0900

    fix

--- setup.py
+++ setup.py
@@ -7,28 +7,35 @@ package_name = "pytchat"
 
 root_dir = path.abspath(path.dirname(__file__))
 
+
 def _requirements():
     return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
+
 
 def _test_requirements():
     return [name.rstrip() for name in open(path.join(root_dir, 'requirements_test.txt')).readlines()]
 
-txt= ''
+
+txt = ''
 # with open('README.MD', 'r', encoding='utf-8') as f:
 #    txt = f.read()
 
 # with open('README1.MD', 'w', encoding='utf-8', newline='\n') as f:
 #    f.write(txt)
-    
+
 # remove("README.MD")
 # rename("README1.MD","README.MD")
 
 with open(path.join(root_dir, package_name, '__init__.py')) as f:
     init_text = f.read()
-    version = re.search(r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
-    license = re.search(r'__license__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
-    author = re.search(r'__author__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
-    author_email = re.search(r'__author_email__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    version = re.search(
+        r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    license = re.search(
+        r'__license__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    author = re.search(
+        r'__author__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    author_email = re.search(
+        r'__author_email__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     url = re.search(r'__url__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
 
 assert version
@@ -38,25 +45,22 @@ assert author_email
 assert url
 
 
-
-
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
-
 setup(
     name=package_name,
-    packages=find_packages(exclude=['*log.txt','*tests']),
+    packages=find_packages(exclude=['*log.txt', '*tests']),
     version=version,
     url=url,
     author=author,
     author_email=author_email,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    # long_description=long_description,
+    # long_description_content_type='text/markdown',
     license=license,
     install_requires=_requirements(),
-    #tests_require=_test_requirements(),
+    # tests_require=_test_requirements(),
     description="a python library for fetching youtube live chat.",
     classifiers=[
         'Natural Language :: Japanese',
@@ -66,7 +70,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',        
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
     keywords='youtube livechat asyncio',
